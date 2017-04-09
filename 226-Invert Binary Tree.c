ec39839c909449b28d50f10ba2ea09edc2e544b7
tanghc10 <738433477@qq.com>
2017-04-09 17:26:33 +0800

    226、371、389

---
+++ 226-Invert Binary Tree.c
@@ -0,0 +1,25 @@
+#include ""stdio.h"
+struct TreeNode* invertTree(struct TreeNode* root);
+int main(void){
+
+}
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if (root == NULL){
+        return NULL;
+    }
+    struct TreeNode *temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
